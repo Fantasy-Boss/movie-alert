25840bbfb1c713eda5d81dd0b0ac35b703fa00f2
Fantasy Boss <apurbo.kumar.official@gmail.com>
2023-05-13 02:15:33 +0600

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const base = "https://api.themoviedb.org/3/"
 const api_key = "1ea9ed9374402446b6208b59da6ec46a"
 
 
-app.use(express.static('web'))
+app.use(express.static(process.cwd() + '/web'))
 app.use(express.json())
 
 
